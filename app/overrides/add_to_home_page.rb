06e78aed0eb20f2a0b726f05365fd2111ac11861
Robert Oles <robertoles@me.com>
2012-08-17 17:17:04 +0100

    Displaying items on home page

--- app/overrides/add_to_home_page.rb
+++ app/overrides/add_to_home_page.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
   virtual_path: "spree/home/index", 
   partial: 'spree/spree_carousels/taxon_carousels',
-  insert_bottom: "[data-hook='homepage_products']",
+  replace: "code[erb-loud]:contains('spree/shared/products')",
   name: 'add_home_page_taxon_carousels')
