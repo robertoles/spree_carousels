661e11a586b5da4361c83cdde4bfe3f5a212566f
Robert Oles <robertoles@me.com>
2012-10-18 14:52:53 +0100

    Using correct path to products partial

--- app/overrides/add_related_products_to_product_display.rb
+++ app/overrides/add_related_products_to_product_display.rb
@@ -3,3 +3,9 @@ Deface::Override.new(
   partial: 'spree/products/related_products',
   insert_bottom: "#product-details",
   name: 'add_product_page_carousel')
+
+Deface::Override.new(
+  virtual_path: "spree/products/show", 
+  partial: 'spree/products/related_products',
+  insert_bottom: "[data-hook='product_show'] .row",
+  name: 'add_product_page_carousel')
